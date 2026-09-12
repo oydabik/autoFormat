64579fb1a126a500c0b1d27d809bdbd7b47867bb
oydabik <vlad.semyonovyx.00@inbox.ru>
2026-09-13 00:53:45 +0400

    Add docx parsing logic and automatic text formatting

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import docx
 from docx.shared import Pt, RGBColor
 from docx.enum.text import WD_ALIGN_PARAGRAPH
+from docx.oxml import OxmlElement
+from docx.oxml.ns import qn
 
 
 class ReportConfig:
@@ -15,54 +17,85 @@ class ReportGenerator:
     def __init__(self):
         self.doc = docx.Document()
 
-    def header_text(self, text):
+    def header_text(self, src_paragraph):
         p = self.doc.add_paragraph()
         p.alignment = WD_ALIGN_PARAGRAPH.CENTER
         p.paragraph_format.space_before = Pt(0)
         p.paragraph_format.space_after = Pt(0)
         p.paragraph_format.line_spacing = 1.5
 
-        run = p.add_run(text)
+        run = p.add_run(src_paragraph.text)
+
         run.font.name = ReportConfig.FONT_MAIN_STYLE
         run.font.size = Pt(ReportConfig.FONT_MAIN_SIZE)
 
-    def main_text(self, text):
+    def main_text(self, src_paragraph):
         p = self.doc.add_paragraph()
         p.alignment = WD_ALIGN_PARAGRAPH.JUSTIFY
         p.paragraph_format.space_before = Pt(0)
         p.paragraph_format.space_after = Pt(0)
         p.paragraph_format.line_spacing = 1.5
+
+        run = p.add_run(src_paragraph.text)
               
-        run = p.add_run(text)
         run.font.name = ReportConfig.FONT_MAIN_STYLE
         run.font.size = Pt(ReportConfig.FONT_MAIN_SIZE)
 
-    def code_text(self,text):
+    def code_text(self, src_paragraph):
         p = self.doc.add_paragraph()
         p.alignment = WD_ALIGN_PARAGRAPH.LEFT
         p.paragraph_format.space_before = Pt(0)
         p.paragraph_format.space_after = Pt(0)
         p.paragraph_format.line_spacing = 1.0
+
+        run = p.add_run(src_paragraph.text)
               
-        run = p.add_run(text)
         run.font.name = ReportConfig.FONT_CODE_STYLE
         run.font.size = Pt(ReportConfig.FONT_CODE_SIZE)
 
-    def parse(self, raw_text):
-        pass
+    def parse(self, input_doc):
+        is_header = True
+        current_mode = "main"
+
+        for paragraph in input_doc.paragraphs:
+            text = paragraph.text
+
+            if not text.strip():
+                self.doc.add_paragraph()
+                continue
+
+            if "Задача" in text or "Выводы" in text:
+                is_header = False
+                current_mode = "main"
+                self.main_text(paragraph)
+                continue
+
+            if "Исходный код" in text:
+                is_header = False
+                current_mode = "code"
+                self.main_text(paragraph)
+                continue
+
+            if is_header:
+                self.header_text(paragraph)
+            elif current_mode == "main":
+                self.main_text(paragraph)
+            elif current_mode == "code":
+                self.code_text(paragraph)
+            
 
     def save(self, file_path):
         self.doc.save(file_path)
 
 if __name__ == "__main__":
     generator = ReportGenerator()
-    generator.header_text("Отчет")
-    generator.main_text("Задача 1. Изучить основы")
-    generator.code_text("print('Hello world')")
 
-    generator.save("text.docx")
+    try:
+        input_doc = docx.Document("Практика01.docx")
 
-    read_doc = docx.Document("text.docx")
-    print("Прочитанные абзацы из файла:")
-    for p in read_doc.paragraphs:
-        print(f"- {p.text}")
+        generator.parse(input_doc)
+        generator.save("FORMATTED.docx")
+        print("Saved!")
+
+    except FileNotFoundError:
+        print("Error!")
